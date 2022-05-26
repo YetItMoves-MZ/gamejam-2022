e8864de9b93784db99791a1b93b489f974f63938
Matan Zvulun <48678786+YetItMoves-MZ@users.noreply.github.com>
2022-05-26 13:54:23 +0300

    finished player respawn (still needs a UI that will call this function)

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -6,18 +6,49 @@ namespace Player
 {
     public class Respawn : MonoBehaviour
     {
-        [SerializeField] private Transform player;
+        [Header("Game Objects")]
+        [SerializeField] private GameObject player;
+        [SerializeField] private GameObject playerBox;
+        [SerializeField] private GameObject Boulder;
+        [SerializeField] private GameObject CielingBeneathBoulder;
+        [SerializeField] private GameObject BoxBeneathBoulder;
+        [Tooltip("make sure both doors are in the same order here as the respawn locations")]
+        [SerializeField] private GameObject[] Doors;
+        [SerializeField] private GameObject PlacedBox;
+        [SerializeField] private GameObject Enemy;
+        [SerializeField] private GameObject Key;
+
+        [Header("Respawn Locations")]
+        [SerializeField] private Transform PlayerSpawner;
+        [SerializeField] private Transform BoulderSpawner;
+        [Tooltip("make sure both doors are in the same order here as the game objects")]
+        [SerializeField] private Transform[] DoorsSpawner;
+
+        [Header("Scripts")]
+        [Tooltip("Player interaction script that is located in the player game object")]
         [SerializeField] private PlayerInteraction playerInteraction;
+        [SerializeField] private UnlocksHandler unlocksHandler;
 
         public void RespawnPlayer()
         {
-            // TODO things that do need to reset after death will be reset here.
-            // LIST OF WHAT TO RESET: boulder, cieling beneath boulder, box beanth boulder, both doors, placed box, ork, key
-            // note: add list of spawn points for everything
-
             playerInteraction.ResetStats();
 
-            this.player.transform.position = this.transform.position;
+            this.player.transform.position = PlayerSpawner.transform.position;
+            this.Boulder.transform.position = BoulderSpawner.transform.position;
+            this.CielingBeneathBoulder.SetActive(true);
+            this.BoxBeneathBoulder.SetActive(true);
+            this.PlacedBox.SetActive(false);
+            this.Enemy.SetActive(true);
+            this.Key.SetActive(false);
+            this.playerBox.SetActive(false);
+
+            unlocksHandler.IsPowerActive[(int)UnlocksHandler.EPowers.Key] = false;
+
+            for (int i = 0; i < Doors.Length; i++)
+            {
+                this.Doors[i].transform.position = this.DoorsSpawner[i].transform.position;
+            }
+
             Physics.SyncTransforms();
         }
     }
